a2efbdc18a016cd9ff820d422bd39141f243fe03
amp7368 <apeterham@gmail.com>
2022-03-11 11:51:44 -0500

    initial commit

---
+++ apps/m-mobs-gen/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="m-mobs-gen" />
+    </StyledApp>
+  );
+}
+
+export default App;
